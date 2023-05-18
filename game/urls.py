dc4eff813928b8871e77425892665f653f01506e
Blindeen <kuba.szmajda@gmail.com>
2023-05-18 17:59:35 +0200

    refactor api urls

--- game/urls.py
+++ game/urls.py
@@ -5,5 +5,6 @@ from game import views
 app_name = "game"
 
 urlpatterns = [
-    path("game/", views.CreateJoinGameSessionView.as_view(), name="create_join_game_session"),
+    path("", views.CreateJoinGameSessionView.as_view(), name="create_join_game_session"),
+    path("start/", views.StartGameSessionView.as_view(), name="start_game_session")
 ]
--- plemiona_api/urls.py
+++ plemiona_api/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("api/v1/", include("game.urls"), name="game"),
+    path("api/v1/game/", include("game.urls"), name="game"),
 ]
