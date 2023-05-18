a26dea17b7f5f34a9fe318658d14b7c036b0bb30
Blindeen <kuba.szmajda@gmail.com>
2023-05-18 18:00:37 +0200

    add starting game endpoint and apply minor changes

--- game/views.py
+++ game/views.py
@@ -7,7 +7,7 @@ from game.models import GameSession, Player, Village
 
 
 class CreateGameSessionSerializer(serializers.Serializer):
-    nickname = serializers.CharField(max_length=30, min_length=6, required=True)
+    nickname = serializers.CharField(max_length=15, min_length=3, required=True)
     game_code = serializers.CharField(max_length=6, required=False, allow_blank=True)
 
 
@@ -19,23 +19,23 @@ class CreateJoinGameSessionView(APIView):
         serializer.is_valid(raise_exception=True)
 
         game_code = serializer.validated_data.pop("game_code", None)
-        if game_code.strip() == "":
+        if game_code and game_code.strip() == "":
             game_code = None
 
         if game_code:
             try:
                 game_session = GameSession.objects.get(game_code=game_code)
             except GameSession.DoesNotExist:
-                return Response({"Game Session": ["Game session does not exist."]}, status=404)
+                return Response({"Game Session": ["does not exist."]}, status=404)
 
             if game_session.has_started:
-                return Response({"Game Session": ["Game has already started."]}, status=400)
+                return Response({"Game Session": ["has already started."]}, status=400)
         else:
             game_session = GameSession.objects.create(owner=None)
 
         player = Player(game_session=game_session, **serializer.validated_data)
         if Player.objects.filter(game_session=game_session, nickname=player.nickname).exists():
-            return Response({"nickname": ["Nickname already in use."]}, status=400)
+            return Response({"Nickname": ["is already in use."]}, status=400)
         player.village = Village.objects.create()
         player.save()
 
@@ -53,3 +53,21 @@ class CreateJoinGameSessionView(APIView):
             "game_code": game_session.game_code,
         }
         return Response(response_data, status=201)
+
+class StartGameSessionView(APIView):
+    def post(self, request, *args, **kwargs):
+
+        player = request.user
+        if player != player.game_session.owner:
+            return Response({"Player": ["is not the owner."]}, status=403)
+
+        if player.game_session.has_started:
+            return Response({"Game session": ["has already started."]}, status=400)
+
+        if player.game_session.player_set.count() < 2:
+            return Response({"Game session": ["requires minimum 2 players to start the game."]}, status=400)
+
+        player.game_session.has_started = True
+        player.game_session.save()
+
+        return Response(status=204)
