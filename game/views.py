3288fbf8b06b9ea77edc952aaeb507e12b3956d2
Damian Mendel <mendeldamian01@gmail.com>
2023-05-19 21:04:41 +0200

    update views

--- game/views.py
+++ game/views.py
@@ -1,13 +1,15 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from game.models import GameSession, Player, Village
+from game.models import GameSession, Player
 
 
 class CreateGameSessionSerializer(serializers.Serializer):
-    nickname = serializers.CharField(max_length=15, min_length=3, required=True)
+    nickname = serializers.CharField(
+        max_length=Player.NICKNAME_MAX_LENGTH, min_length=Player.NICKNAME_MIN_LENGTH, required=True
+    )
     game_code = serializers.CharField(max_length=6, required=False, allow_blank=True)
 
 
@@ -18,6 +20,7 @@ class CreateJoinGameSessionView(APIView):
         serializer = CreateGameSessionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
+        nickname = serializer.validated_data["nickname"]
         game_code = serializer.validated_data.pop("game_code", None)
         if game_code and game_code.strip() == "":
             game_code = None
@@ -26,18 +29,16 @@ class CreateJoinGameSessionView(APIView):
             try:
                 game_session = GameSession.objects.get(game_code=game_code)
             except GameSession.DoesNotExist:
-                return Response({"Game Session": ["does not exist."]}, status=404)
+                return Response({"Game Session": ["does not exist."]}, status=status.HTTP_404_NOT_FOUND)
 
             if game_session.has_started:
-                return Response({"Game Session": ["has already started."]}, status=400)
+                return Response({"Game Session": ["has already started."]}, status=status.HTTP_400_BAD_REQUEST)
         else:
             game_session = GameSession.objects.create(owner=None)
 
-        player = Player(game_session=game_session, **serializer.validated_data)
-        if Player.objects.filter(game_session=game_session, nickname=player.nickname).exists():
-            return Response({"Nickname": ["is already in use."]}, status=400)
-        player.village = Village.objects.create()
-        player.save()
+        if Player.objects.filter(game_session=game_session, nickname=nickname).exists():
+            return Response({"Nickname": ["is already in use."]}, status=status.HTTP_400_BAD_REQUEST)
+        player = Player.objects.create(game_session=game_session, nickname=nickname)
 
         if not game_session.owner:
             game_session.owner = player
@@ -47,19 +48,19 @@ class CreateJoinGameSessionView(APIView):
 
         refresh = RefreshToken.for_user(player)
         response_data = {
-            'token': str(refresh.access_token),
-            'player_id': player.id,
-            'game_session_id': game_session.id,
+            "token": str(refresh.access_token),
+            "player_id": player.id,
+            "game_session_id": game_session.id,
             "game_code": game_session.game_code,
         }
-        return Response(response_data, status=201)
+        return Response(response_data, status=status.HTTP_201_CREATED)
+
 
 class StartGameSessionView(APIView):
     def post(self, request, *args, **kwargs):
-
         player = request.user
         if player != player.game_session.owner:
-            return Response({"Player": ["is not the owner."]}, status=403)
+            return Response({"Player": ["is not the owner."]}, status=status.HTTP_403_FORBIDDEN)
 
         if player.game_session.has_started:
             return Response({"Game session": ["has already started."]}, status=400)
@@ -70,4 +71,4 @@ class StartGameSessionView(APIView):
         player.game_session.has_started = True
         player.game_session.save()
 
-        return Response(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
