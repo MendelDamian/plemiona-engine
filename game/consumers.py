21c2ae12016513939dfc5de6631e249f57651433
Blindeen <kuba.szmajda@gmail.com>
2023-05-23 20:55:35 +0200

    add players list broadcast event after connecting to socket

--- game/consumers.py
+++ game/consumers.py
@@ -2,9 +2,16 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
+from game.models import Player
+from game.serializers import PlayerSerializer
+
 class LobbyConsumer(WebsocketConsumer):
     def connect(self):
-        self.room_group_name = "lobby"
+        player = self.scope.get("player", None)
+        if not player:
+            self.close()
+
+        self.room_group_name = player.game_session.game_code
 
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
@@ -13,27 +20,33 @@ class LobbyConsumer(WebsocketConsumer):
 
         self.accept()
 
+        self.send_players_list(player)
+
+
+    def players_list(self, event):
+        players_list = event["players_list"]
+        owner = event["owner"]
+
+        players_list_serializer = PlayerSerializer(players_list, many=True)
+        owner_serializer = PlayerSerializer(owner)
+
+        data = {
+            "owner": owner_serializer.data,
+            "players": players_list_serializer.data
+        }
         self.send(text_data=json.dumps({
-            "type": "connection established",
-            "message": "You are now connected!"
+            "type": "players_list",
+            "data": data
         }))
 
-    def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+    def send_players_list(self, player):
+        players_list = Player.objects.filter(game_session=player.game_session)
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                "type": "chat_message",
-                "message": message
+                "type": "players_list",
+                "players_list": players_list,
+                "owner": player.game_session.owner
             }
         )
-
-    def chat_message(self, event):
-        message = event["message"]
-
-        self.send(text_data=json.dumps({
-            "type": "chat",
-            "message": message
-        }))
