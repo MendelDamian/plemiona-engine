658c3ff11ad8a7238b74ce094c9b8b0dc8fa87c7
Blindeen <kuba.szmajda@gmail.com>
2023-05-23 20:47:10 +0200

    remove unused import

--- plemiona_api/management.py
+++ plemiona_api/management.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
 import jwt
-from django.conf import settings
 from rest_framework import authentication
 from rest_framework.exceptions import AuthenticationFailed
 
