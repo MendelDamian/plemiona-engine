9a15d073ef6bb4d5e7f2c537534759b5e3dc3557
Blindeen <kuba.szmajda@gmail.com>
2023-05-23 17:43:40 +0200

    configure server settings

--- plemiona_api/settings.py
+++ plemiona_api/settings.py
@@ -27,6 +27,7 @@ CORS_ALLOWED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,6 +39,14 @@ INSTALLED_APPS = [
     "corsheaders",
     "game",
 ]
+
+ASGI_APPLICATION = "plemiona_api.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
