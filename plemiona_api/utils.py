a13cabda3bc8f08c72de33d6cd351cbf785caa39
Damian Mendel <mendeldamian01@gmail.com>
2023-05-24 09:13:52 +0200

    delete try except block

--- plemiona_api/utils.py
+++ plemiona_api/utils.py
@@ -1,17 +1,23 @@
 from rest_framework.exceptions import APIException
 from rest_framework.views import exception_handler
 
+
+def prep(code):
+    return code.replace("_", " ").capitalize()
+
+
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
-    try:
-        if response is not None and isinstance(exc, APIException):
-            data = {"errors": {}}
-            for field, errors in exc.detail.items():
-                for error in errors:
-                    data["errors"][field.capitalize()] = [error.capitalize()]
-            response.data = data
-    except:
-        pass
+    if not response or not isinstance(exc, APIException):
+        return response
+
+    response.data = {"errors": {}}
+    if isinstance(exc.detail, (list, dict)):
+        for field, errors in exc.detail.items():
+            for error in errors:
+                response.data["errors"][prep(field)] = [prep(error)]
+    else:
+        response.data["errors"][prep(exc.detail.code)] = [prep(exc.detail)]
 
     return response
