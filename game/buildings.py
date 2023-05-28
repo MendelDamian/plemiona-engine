6cac003b1632e91adc948c2f476395c19dc2a8e4
Damian Mendel <mendeldamian01@gmail.com>
2023-05-28 20:47:11 +0200

    refactor buildings' fields to constants

--- game/buildings.py
+++ game/buildings.py
@@ -1,28 +1,27 @@
-from dataclasses import dataclass
 from datetime import timedelta
 from typing import ClassVar
 
 
-@dataclass
 class Building:
     MAX_LEVEL: ClassVar[int] = 1
-    level: int = 1
-    base_upgrade_time: timedelta = timedelta(minutes=1)
+    BASE_UPGRADE_TIME: ClassVar[timedelta] = timedelta(minutes=1)
 
     # Upgrade costs
-    wood_cost: int = 0
-    clay_cost: int = 0
-    iron_cost: int = 0
+    WOOD_COST: ClassVar[int] = 0
+    CLAY_COST: ClassVar[int] = 0
+    IRON_COST: ClassVar[int] = 0
+
+    level: int = 1
 
     def get_upgrade_cost(self) -> dict:
         return {
-            "wood": self.wood_cost * self.level,
-            "clay": self.clay_cost * self.level,
-            "iron": self.iron_cost * self.level,
+            "wood": self.WOOD_COST * self.level,
+            "clay": self.CLAY_COST * self.level,
+            "iron": self.IRON_COST * self.level,
         }
 
     def get_upgrade_time(self) -> timedelta:
-        return self.base_upgrade_time * self.level
+        return self.BASE_UPGRADE_TIME * self.level
 
     def upgrade(self) -> None:
         if self.level < self.MAX_LEVEL:
@@ -35,94 +34,88 @@ class Building:
 
 class ResourceBuilding(Building):
     # Producation per second
-    wood_production: float = 0
-    clay_production: float = 0
-    iron_production: float = 0
+    WOOD_PRODUCATION: ClassVar[float] = 0
+    CLAY_PRODUCATION: ClassVar[float] = 0
+    IRON_PRODUCATION: ClassVar[float] = 0
 
     def get_production(self) -> dict:
         return {
-            "wood": self.wood_production * self.level,
-            "clay": self.clay_production * self.level,
-            "iron": self.iron_production * self.level,
+            "wood": self.WOOD_PRODUCATION * self.level,
+            "clay": self.CLAY_PRODUCATION * self.level,
+            "iron": self.IRON_PRODUCATION * self.level,
         }
 
 
-@dataclass
 class TownHall(Building):
     MAX_LEVEL = 3
-    base_upgrade_time: timedelta = timedelta(minutes=10)
+    BASE_UPGRADE_TIME = timedelta(minutes=10)
 
     # Costs
-    wood_cost: int = 200
-    clay_cost: int = 170
-    iron_cost: int = 90
+    WOOD_COST = 200
+    CLAY_COST = 170
+    IRON_COST = 90
 
 
-@dataclass
 class Granary(Building):
     MAX_LEVEL = 3
-    base_upgrade_time: timedelta = timedelta(minutes=3)
+    BASE_UPGRADE_TIME = timedelta(minutes=3)
 
     # Costs
-    wood_cost: int = 100
-    clay_cost: int = 100
-    iron_cost: int = 100
+    WOOD_COST = 100
+    CLAY_COST = 100
+    IRON_COST = 100
 
     # Capacity
-    capacity: int = 1000
+    CAPACITY: ClassVar[int] = 1000
 
     def get_capacity(self) -> int:
-        return self.capacity * self.level
+        return self.CAPACITY * self.level
 
 
-@dataclass
 class IronMine(ResourceBuilding):
     MAX_LEVEL = 3
-    base_upgrade_time: timedelta = timedelta(minutes=5)
+    BASE_UPGRADE_TIME = timedelta(minutes=5)
 
     # Costs
-    wood_cost: int = 100
-    clay_cost: int = 80
-    iron_cost: int = 30
+    WOOD_COST = 100
+    CLAY_COST = 80
+    IRON_COST = 30
 
     # Producation per second
-    iron_production: float = 0.5
+    IRON_PRODUCATION = 0.5
 
 
-@dataclass
 class ClayPit(ResourceBuilding):
     MAX_LEVEL = 3
-    base_upgrade_time: timedelta = timedelta(minutes=5)
+    BASE_UPGRADE_TIME = timedelta(minutes=5)
 
     # Costs
-    wood_cost: int = 30
-    clay_cost: int = 80
-    iron_cost: int = 100
+    WOOD_COST = 30
+    CLAY_COST = 80
+    IRON_COST = 100
 
     # Producation per second
-    clay_production: float = 0.5
+    CLAY_PRODUCATION = 0.5
 
 
-@dataclass
 class Sawmill(ResourceBuilding):
     MAX_LEVEL = 3
-    base_upgrade_time: timedelta = timedelta(minutes=5)
+    BASE_UPGRADE_TIME = timedelta(minutes=5)
 
     # Costs
-    wood_cost: int = 30
-    clay_cost: int = 100
-    iron_cost: int = 80
+    WOOD_COST = 30
+    CLAY_COST = 100
+    IRON_COST = 80
 
     # Producation per second
-    wood_production: float = 0.5
+    WOOD_PRODUCATION = 0.5
 
 
-@dataclass
 class Barracks(Building):
     MAX_LEVEL = 3
-    base_upgrade_time: timedelta = timedelta(minutes=5)
+    BASE_UPGRADE_TIME = timedelta(minutes=5)
 
     # Costs
-    wood_cost: int = 200
-    clay_cost: int = 170
-    iron_cost: int = 90
+    WOOD_COST = 200
+    CLAY_COST = 170
+    IRON_COST = 90
