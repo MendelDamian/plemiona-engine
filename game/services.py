76d62e2543094e7a8227e0342fa4e4608f735e40
Damian Mendel <mendeldamian01@gmail.com>
2023-06-04 19:24:41 +0200

    update serializers and services to meet the standards

--- game/services.py
+++ game/services.py
@@ -4,51 +4,45 @@ from django.utils import timezone
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
-from game import exceptions
-from game.consumers import GameConsumer
+from game import exceptions, serializers
 from game.models import GameSession, Player, Village
 
 
 class GameSessionConsumerService:
     @staticmethod
-    def send_start_game_session(game_session):
-        game_consumer = GameConsumer()
-        game_consumer.game_session = game_session
-        game_consumer.room_group_name = str(game_session.game_code)
-        game_consumer.channel_layer = get_channel_layer()
-
-        async_to_sync(game_consumer.channel_layer.group_send)(
-            game_consumer.room_group_name,
-            {
-                "type": "start_game_session",
+    def send_start_game_session(game_session: GameSession):
+        data = {
+            "type": "start_game_session",
+            "data": {
+                "players": serializers.PlayerDataSerializer(game_session.player_set.all(), many=True).data,
             },
-        )
+        }
+        GameSessionConsumerService._send_message(game_session.game_code, data)
 
     @staticmethod
-    def send_fetch_resources(player):
-        game_consumer = GameConsumer()
-        game_consumer.player = player
-        game_consumer.player_group_name = str(player.channel_name)
-        game_consumer.channel_layer = get_channel_layer()
-
-        async_to_sync(game_consumer.channel_layer.group_send)(
-            game_consumer.player_group_name,
-            {
-                "type": "fetch_resources",
-            },
-        )
+    def send_fetch_resources(player: Player):
+        data = {
+            "type": "fetch_resources",
+            "data": serializers.ResourcesSerializer(player.village).data,
+        }
+        GameSessionConsumerService._send_message(player.channel_name, data)
 
     @staticmethod
-    def send_fetch_buildings(player):
-        game_consumer = GameConsumer()
-        game_consumer.player = player
-        game_consumer.player_group_name = str(player.channel_name)
-        game_consumer.channel_layer = get_channel_layer()
+    def send_fetch_buildings(player: Player):
+        data = {
+            "type": "fetch_buildings",
+            "data": serializers.VillageSerializer(player.village).data,
+        }
+        GameSessionConsumerService._send_message(player.channel_name, data)
 
-        async_to_sync(game_consumer.channel_layer.group_send)(
-            game_consumer.player_group_name,
+    @staticmethod
+    def _send_message(channel_name, data):
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            str(channel_name),
             {
-                "type": "fetch_buildings",
+                "type": "send_message",
+                "data": data,
             },
         )
 
@@ -142,7 +136,6 @@ class VillageService:
 
 
 class CoordinateService:
-    MAP_SIZE = 32
     AVAILABLE_TILES = (
         (0, 0),
         (0, 1),
