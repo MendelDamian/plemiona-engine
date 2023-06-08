3bb80d25eed3042487aaebaf05b1db70e19af56b
Damian Mendel <mendeldamian01@gmail.com>
2023-06-08 09:29:40 +0200

    add view to urls

--- game/urls.py
+++ game/urls.py
@@ -7,5 +7,6 @@ app_name = "game"
 urlpatterns = [
     path("", views.CreateJoinGameSessionView.as_view(), name="create_join_game_session"),
     path("start/", views.StartGameSessionView.as_view(), name="start_game_session"),
-    path("building/<str:building_name>/upgrade/", views.UpgradeBuildingView.as_view(), name="upgrade_building")
+    path("building/<str:building_name>/upgrade/", views.UpgradeBuildingView.as_view(), name="upgrade_building"),
+    path("train_units/", views.TrainUnitsView.as_view(), name="train_units"),
 ]
