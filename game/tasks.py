0a96de3c218eb9b6842ca306ddc0626f64f2df79
Blindeen <kuba.szmajda@gmail.com>
2023-06-08 18:29:23 +0200

    apply task registration

--- game/tasks.py
+++ game/tasks.py
@@ -1,24 +1,25 @@
 from time import sleep
 from collections import OrderedDict
 
-from game import exceptions
-from game.models import Player, Village, GameSession
+from game import exceptions, models
 from plemiona_api.celery import app
 
 
-@app.task
-def upgrade_building_task(player_id, building_name, seconds):
+@app.task(bind=True)
+def upgrade_building_task(self, player_id, building_name, seconds):
     from game.services import GameSessionConsumerService
 
-    if building_name not in Village.BUILDING_NAMES:
+    if building_name not in models.Village.BUILDING_NAMES:
         raise exceptions.BuildingNotFoundException
 
-    player = Player.objects.get(id=player_id)
+    player = models.Player.objects.get(id=player_id)
     player.village.set_building_upgrading_state(building_name, True)
+
+    current_task = models.Task.objects.create(game_session=player.game_session, task_id=self.request.id)
 
     sleep(seconds)
 
-    refreshed_player = Player.objects.get(id=player_id)
+    refreshed_player = models.Player.objects.get(id=player_id)
     refreshed_player.village.update_resources()
 
     refreshed_player.village.upgrade_building_level(building_name)
@@ -27,6 +28,8 @@ def upgrade_building_task(player_id, building_name, seconds):
     GameSessionConsumerService.send_fetch_buildings(refreshed_player)
     GameSessionConsumerService.send_fetch_resources(refreshed_player)
 
+    current_task.has_ended = True
+
 
 @app.task
 def send_leaderboard_task(game_session_id, seconds):
@@ -34,18 +37,26 @@ def send_leaderboard_task(game_session_id, seconds):
 
     sleep(seconds)
 
-    game_session = GameSession.objects.get(id=game_session_id)
+    game_session = models.GameSession.objects.get(id=game_session_id)
+
+    for task in game_session.task_set.all():
+        if not task.has_ended:
+            app.control.revoke(task.task_id, terminate=True)
+            task.has_ended = True
+
     GameSessionConsumerService.send_fetch_leaderboard(game_session)
 
 
-@app.task
-def train_units_task(player_id, units_to_train: list[OrderedDict]):
+@app.task(bind=True)
+def train_units_task(self, player_id, units_to_train: list[OrderedDict]):
     from game.units import UNITS
     from game.services import GameSessionConsumerService
 
-    player = Player.objects.get(id=player_id)
+    player = models.Player.objects.get(id=player_id)
     player.village.are_units_training = True
     player.village.save()
+
+    current_task = models.Task.objects.create(game_session=player.game_session, task_id=self.request.id)
 
     for unit in units_to_train:
         unit_name, unit_count = unit["name"], unit["count"]
@@ -55,11 +66,13 @@ def train_units_task(player_id, units_to_train: list[OrderedDict]):
         for _ in range(unit_count):
             sleep(unit_trainig_time)
 
-            refreshed_player = Player.objects.get(id=player_id)
+            refreshed_player = models.Player.objects.get(id=player_id)
             refreshed_player.village.increase_unit_count(unit_name, 1)
 
             GameSessionConsumerService.send_fetch_units_count(refreshed_player)
 
-    refreshed_player = Player.objects.get(id=player_id)
+    refreshed_player = models.Player.objects.get(id=player_id)
     refreshed_player.village.are_units_training = False
     refreshed_player.village.save()
+
+    current_task.has_ended = True
