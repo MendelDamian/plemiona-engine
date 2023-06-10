ecb0f5bbeae808733cb05ea78c1eeebaed07d94f
Damian Mendel <mendeldamian01@gmail.com>
2023-06-10 23:26:02 +0200

    add all models to admin.py

--- game/admin.py
+++ game/admin.py
@@ -1,24 +1,10 @@
 from django.contrib import admin
 
-from .models import GameSession, Player, Village
+from game import models
 
 
-class PlayerInline(admin.TabularInline):
-    model = Player
-    extra = 0
-
-
-class GameSessionAdmin(admin.ModelAdmin):
-    list_display = ("game_code", "has_started", "owner")
-    inlines = [
-        PlayerInline,
-    ]
-
-
-class PlayerAdmin(admin.ModelAdmin):
-    list_display = ("nickname", "game_session", "village")
-
-
-admin.site.register(GameSession, GameSessionAdmin)
-admin.site.register(Player, PlayerAdmin)
-admin.site.register(Village)
+admin.site.register(models.GameSession)
+admin.site.register(models.Task)
+admin.site.register(models.Player)
+admin.site.register(models.Village)
+admin.site.register(models.Battle)
