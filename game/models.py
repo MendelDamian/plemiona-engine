784f063c4f371b3b716e3567210fdce6dd9919bb
Damian Mendel <mendeldamian01@gmail.com>
2023-06-15 07:49:12 +0200

    delete bonus points field for player, add morale as bonus points

--- game/models.py
+++ game/models.py
@@ -57,7 +57,6 @@ class Player(BaseModel):
 
     nickname = models.CharField(max_length=NICKNAME_MAX_LENGTH, null=False)
     channel_name = models.UUIDField(default=uuid.uuid4, editable=False, null=False)
-    bonus_points = models.IntegerField(default=0, null=False)
 
     game_session = models.ForeignKey("GameSession", on_delete=models.CASCADE, null=False)
     village = models.OneToOneField("Village", on_delete=models.CASCADE, null=False)
@@ -71,7 +70,7 @@ class Player(BaseModel):
     def points(self):
         building_points = sum([building.points for _, building in self.village.buildings.items()])
         unit_points = sum([unit.points for _, unit in self.village.units.items()])
-        return building_points + unit_points
+        return building_points + unit_points + self.village.morale
 
     def save(self, *args, **kwargs):
         # Check if the player has a village, avoid RelatedObjectDoesNotExist
