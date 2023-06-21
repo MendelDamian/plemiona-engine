40aa9733462e5c5587e9c5b1b3a85e0625b8d514
Damian Mendel <mendeldamian01@gmail.com>
2023-06-21 21:42:57 +0200

    move static dir to project directory

--- plemiona_api/settings.py
+++ plemiona_api/settings.py
@@ -131,7 +131,7 @@ LOGIN_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(BASE_DIR), "static"))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 STATIC_URL = "/static/"
 STATICFILES_FINDERS = (
@@ -140,7 +140,7 @@ STATICFILES_FINDERS = (
 )
 
 # Media files
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 # Default primary key field type
