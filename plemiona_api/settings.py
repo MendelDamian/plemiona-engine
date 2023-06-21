8a74585b987f920bd5ff7d889c201e7b5323b63e
Damian Mendel <mendeldamian01@gmail.com>
2023-06-21 21:45:12 +0200

    run black

--- plemiona_api/settings.py
+++ plemiona_api/settings.py
@@ -131,7 +131,7 @@ LOGIN_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = []
 STATIC_URL = "/static/"
 STATICFILES_FINDERS = (
